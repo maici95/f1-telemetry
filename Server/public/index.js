51d14353bca1919025be38d12e64b575294562f4
maici95 <mikajmarjomaa@gmail.com>
2020-11-07 22:04:36 +0200

    wheel display layout

--- Server/public/index.js
+++ Server/public/index.js
@@ -1,7 +1,7 @@
 
 
 
-const UPDATE_FREQ = 1000 / 60;
+const UPDATE_FREQ = 1000 / 5;
 
 function fetchData() {
     return new Promise((resolve, reject) => {
@@ -45,25 +45,95 @@ function App() {
         }
     });
 
+    // Last lap delta
+    const [miniIndex, setMiniIndex] = React.useState(0);
+    const [lastLap, setLastLap] = React.useState([]);
+    const [currentLap, setCurrentLap] = React.useState([]);
+    const [lapNum, setLapNum] = React.useState(null);
+    const [lapDelta, setLapDelta] = React.useState(0);
+
+    React.useEffect(() => {
+        if (lapData.lapDistance > miniIndex * 100) {
+            setMiniIndex(miniIndex + 1);
+            setCurrentLap([...currentLap, lapData.currentLapTime]);
+        }
+
+        if (lapNum !== lapData.currentLapNum) {
+            setLapNum(lapData.currentLapNum);
+            setLastLap([...currentLap]);
+            setMiniIndex(0);
+            setCurrentLap([]);
+        }
+
+        if (lastLap.length > 0 && currentLap.length > 0) {
+            const index = currentLap.length - 1;
+            setLapDelta((lastLap[index] - currentLap[index]).toFixed(2));
+        }
+    }, [session]);
+
 
     return (
         <div>
-            packet format: {header.packetFormat}
-            <br/>
-            world x: {motion.worldPositionX}
-            <br/>
-            time left: {session.test && session.test.sessionTimeLeft}
-            <br/>
-            last lap: {lapData.lastLapTime}
-            <br/>
-            fuel load: {carSetup.fuelLoad}
-            <br/>
-            speed: {carTelemetry.speed}
-            <br/>
-            fuel mix: {carStatus.fuelMix}
+            <LEDPanel></LEDPanel>
+            <LinePanel>
+
+            </LinePanel>
+            <div>
+                <Column size="m"></Column>
+                <Column size="l">
+                    <div className="big">
+                        {carTelemetry.gear || 'N'}
+                    </div>
+                    <div className="small">
+                        {carTelemetry.speed || 0}
+                    </div>
+                </Column>
+                <Column size="m">
+                    <div className="small" style={{color: lapDelta > 0 ? 'green' : 'red'}}>
+                        {lapDelta}
+                    </div>
+                </Column>
+                <Column size="s">
+                    <div style={{width:'100%', height:(100 - (carStatus.ersStoreEnergy / 4000000) * 100)+'%'}}></div>
+                    <div className='ers-bar' style={{width:'100%', height:(carStatus.ersStoreEnergy / 4000000) * 100+'%'}}></div>
+                </Column>
+            </div>
         </div>
     );
 }
+
+
+function LEDPanel() {
+    return (
+        <div className="led-panel">
+            
+        </div>
+    );
+}
+function LinePanel(props) {
+    return (
+        <div className="line-panel">
+            {props.children}
+        </div>
+    );
+}
+function Column(props) {
+    const colWidth =
+          props.size === 'l' ? '35vw'
+        : props.size === 'm' ? '30vw'
+        : props.size === 's' ? '5vw'
+        : '20vw';
+
+    const style = {
+        width: colWidth
+    }
+    return (
+        <div className="column" style={style}>
+            {props.children}
+        </div>
+    );
+}
+
 
 ReactDOM.render(
     <App />,
